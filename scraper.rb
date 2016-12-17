e1ce3b8589aa5d93b87b0e7ce5bd562b04f79ce3
Tony Bowden <tony@tmtm.com>
2016-12-17 09:46:01 +0000

    Remove batch processing

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,5 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/china-national-peoples-congress', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { zh: names }, batch_size: 100)
+EveryPolitician::Wikidata.scrape_wikidata(names: { zh: names })
 
