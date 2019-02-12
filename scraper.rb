74adcf3b089e10c5a4aca7ebaebe74a4ca6a1edf
Tony Bowden <tony@tmtm.com>
2019-02-12 12:54:22 +0000

    Also get subclasses of "member of the National People's Congress"
    e.g. Q51139412 is "standing member of the National People's Congress"

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,6 @@
 
 require 'wikidata/fetcher'
 
-sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 wd:Q10891456 ; pq:P2937 wd:Q247766 ] }'
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39/wdt:P279* wd:Q10891456 ; pq:P2937 wd:Q247766 ] }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
